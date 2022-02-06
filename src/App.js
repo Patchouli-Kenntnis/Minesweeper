a8ff87df8bd9f3a9c29449364354065d084f5de8
Hibernating Patchouli <hibernating_patchouli@protonmail.com>
2022-02-06 17:21:52 +0100

    basic grid display

--- src/App.js
+++ src/App.js
@@ -1,12 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { nanoid } from 'nanoid';
 
 function App() {
   return (
     <div className="App">
       <h1>War, War never changes.</h1>
+
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Game
+          rows={15}
+          columns={20}
+        />
+      </div>
+
     </div>
   );
 }
 
+function Block(props) {
+  return (
+    <button className="Block" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+function Grid(props) {
+
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rows: props.rows,
+      columns: props.columns,
+      grid: Array(props.rows).fill(0).map(x => Array(props.columns).fill(0))
+    };
+
+
+  }
+  render() {
+    let rows = //(<tr><td>{this.state.rows}</td></tr>)  ;
+      this.state.grid.map(
+        (row) => {
+          let key = 0;
+          return (
+            <tr key={nanoid()}>
+              {
+                row.map(
+                  (elem) =>
+                    (<td key={nanoid()}><Block value="9" /></td>)
+                )
+              }
+            </tr>
+          )
+        }
+
+      )
+    return (
+      <table>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    );
+  }
+}
 export default App;
